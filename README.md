ab6dbdc1a39b1c0fc0206488881c5c6c24c22ac5
cnjoanne <scn153@gmail.com>
2023-07-16 04:10:42 +0800

    update: playing around (i am procrastinating, help)

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Blind-mans-game
+
+## Steps
+1. pip install pygame
+2. run main.py
+
+## TODOS:
+1. Add png files into data [blind person, cars, road, bushes(optional), traffic light(optional)]
+2. Add sounds of cars 
+3. ++
+
+
+## Extra notes -cn
+following link : https://www.youtube.com/watch?v=jO6qQDNa2UY \
+reference game: https://github.com/Gooodgis/dont-touch-my-presents/blob/main/README.md
---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+
+WIDTH, HEIGHT = 300,500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("First game")
+
+WHITE = (255, 255, 255)
+
+## Frame per second
+FPS = 60
+
+BLINDMAN_HEIGHT, BLINDMAN_WIDTH = 3, 5
+## Images
+BLINDMAN_IMAGE = pygame.image.load(
+    os.path.join('data', 'blindman.png'))
+## scaling
+BLINDMAN = pygame.transform.rotate(pygame.transform_scale(BLINDMAN_IMAGE, (BLINDMAN_HEIGHT, BLINDMAN_WIDTH)), 90) # image, width by height || angle rotate
+CAR_IMAGE = pygame.image.load(
+    os.path.join('data', 'car.png'))
+ROAD_IMAGE = pygame.image.load(
+    os.path.join('data', 'road.png'))
+
+# drawings, they are squential
+def draw_window():
+        WIN.fill(WHITE)
+        # coordinates start from top left hand corner
+        WIN.blit(BLINDMAN, (0,0)) ## image, coordinate
+        pygame.display.update()
+
+
+def main():
+    clock = pygame.time.Clock() ## control time 
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.typye == pygame.QUIT:
+                run = False
+
+        draw_window()
+    pygame.quit()
+
+if __name__ == "main":
+    main()
