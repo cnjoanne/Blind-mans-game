af96f0e91d7b9570e2cecac3445d078f117df31e
cnjoanne <scn153@gmail.com>
2023-07-21 16:14:29 +0800

    feat: blindman can move

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("First game")
 
 WHITE = (255, 255, 255)
+VEL = 5 # Velocity
 
 
 ## Frame per second
@@ -26,23 +27,28 @@ ROAD = pygame.image.load(
     os.path.join('data', 'assets', 'road.png'))
 ROAD_IMAGE = pygame.transform.scale(ROAD, (WIDTH,HEIGHT))
 
-def blindman_movement(key_pressed, blindman):
-     pass
+def blindman_movement(keys_pressed, blindman):
+    if keys_pressed[pygame.K_a]: # LEFT
+        blindman.x -= VEL
+    if keys_pressed[pygame.K_d]: # RIGHT
+        blindman.x += VEL
 
 def car_movements():
      pass
 
 # drawings, they are squential
-def draw_window():
+def draw_window(blindman):
         WIN.fill(WHITE)
         # coordinates start from top left hand corner
         WIN.blit(ROAD_IMAGE, (0,0))
-        WIN.blit(BLINDMAN, (10, HEIGHT - BLINDMAN_HEIGHT -10)) ## image, coordinate
+        ## WIN.blit(BLINDMAN, (10, HEIGHT - BLINDMAN_HEIGHT -10)) ## image, coordinate
+        WIN.blit(BLINDMAN, (blindman.x, HEIGHT - BLINDMAN_HEIGHT -10)) ## image, coordinate
         WIN.blit(CAR_IMAGE, (10,10))
         pygame.display.update()
 
 
 def main():
+    blindman = pygame.Rect(10, 10, BLINDMAN_WIDTH, BLINDMAN_HEIGHT)
     clock = pygame.time.Clock() ## control time
     run = True
     try:
@@ -52,8 +58,13 @@ def main():
                 if event.type == pygame.QUIT:
                     run = False
 
-            draw_window()
-            pygame.display.update()
+            
+            keys_pressed = pygame.key.get_pressed()
+            blindman_movement(keys_pressed, blindman)
+           
+
+            draw_window(blindman)
+            # pygame.display.update()
         pygame.quit()
     except SystemExit:
          pygame.quit()
--- src/components/incoming_car.py
+++ src/components/incoming_car.py
@@ -7,3 +7,8 @@ from src.components.car_side import CarSide
 class Car(pygame.sprite.Sprite):
      def __init__(self, car_side, Carside):
           super().__init__()
+          self.new_speed = random.uniform(2,3.5)
+          self.new_y = 0
+          self.offset_x = 0
+          self.new_x = sine(100.0, 1280, 20.0, self.offset_x)
+          self.side = car_side
